0831a50fcfe071922029dd2db2e3af1797a26d2c
Your Name <keanan09hong@gmail.com>
2025-03-21 10:55:00 +0800

    test commit - keanan

--- SC2002_BTO/src/BTO/Users.java
+++ SC2002_BTO/src/BTO/Users.java
@@ -6,12 +6,14 @@ public class Users {
 	private String password = "password";
 	private int age;
 	private boolean married;
+	private String keanan;
 
 	public Users(String nric, String password, int age, boolean married) {
 		this.nric = nric;
 		this.password = password;
 		this.age = age;
 		this.married = married;
+		this.keanan = "hi";
 	}
 
 }
