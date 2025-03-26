b8367405f5b31408a6a45ac37028a36b319e08a8
junno <junno@172.20.105.170>
2025-03-26 22:26:33 +0800

    updated application class with get and sets

--- SC2002_BTO/src/BTO/Application.java
+++ SC2002_BTO/src/BTO/Application.java
@@ -1,10 +1,35 @@
 package BTO;
 
 public class Application {
-	//junn was here
-	int smth = 1;
+	private Applicant applicant; 
+	private int roomType; 
+	private String status; 
+	
 	public Application() {
-		// TODO Auto-generated constructor stub
+		this.applicant = new Applicant();
 	}
-
+	
+	public Applicant getApplicant() {
+		return this.applicant; 
+	}
+	
+	public int getRoomType() {
+		return this.roomType;
+	}
+	
+	public int setRoomType(int roomType) {
+		if (roomType == 2 || roomType == 3) {
+			this.roomType = roomType;
+			return 0;
+		}
+		return -1; 
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status; 
+	}
 }
